c1faa068d8b800cbbf7fd85c73cfbda776202c2a
yeezick <ramerick5@gmail.com>
2021-09-04 14:04:22 -0400

    creatign a file structure for lectures to come; completing what-is-react

---
+++ section-3/what-is-react.js
@@ -0,0 +1,28 @@
+/**
+ * WHAT IS REACT 
+ * 
+ * React is front-end UI framework, made to simplify the process of building complex sites, in comparison to building the same sites using vanilla html, css, and js.
+* React is al about COMPONENTS, because all user interfaces in the end are made up of components.  
+* Components are resusable coding blocks for the user interface. Ina finance tracking system, each statement would be a component. 1 piece of code block that can very easily vary the rendered data
+*/
+
+/* WHY COMPONENTS
+* Reusability 
+  - Don't repeat yourself
+* Separation of Concerns
+  - Keeps our codebase small and mangeable 
+  - Don't do too many things in one and the same place
+* Maintaing clean, dry code is the craft of programming itself. In every screen and function, we are splitting big chunks of code into multiple smaller functions.
+* 
+ */
+
+/**
+ * HOW IS A COMPONENT BUILT
+ * HTML, CSS, JS make up React.
+ * 
+ * React uses a declarative approach. 
+  - It allows us to define the desired end result and use it at will, in coparison to a static site which needs content defined in its place. 
+  - Essentially building custom HTML elements
+  *
+ *
+ */
